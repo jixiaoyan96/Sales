5a5f3f96b328e511c7f1b377e068054d8f149e59
anxiaxia <1286571499@qq.com>
2019-03-21 14:56:55 +0800

    销售转移显示

--- sales/protected/commands/SaleCommand.php
+++ sales/protected/commands/SaleCommand.php
@@ -5,8 +5,8 @@ class SaleCommand extends CConsoleCommand
     {
         $suffix = Yii::app()->params['envSuffix'];
         $sql="UPDATE sales$suffix.sal_visit a ,security$suffix.sec_city b ,security$suffix.sec_user c 
-              set a.username=b.incharge  and a.shift='Y'          
-              WHERE a.city=b.code AND a.username=c.username AND a.city=c.city AND c.status='I';";
+              set a.username=b.incharge , a.shift='Y'          
+              WHERE a.city=b.code AND a.username=c.username AND a.city=c.city AND c.status='I'";
         $command=Yii::app()->db->createCommand($sql)->execute();
     }
 }
