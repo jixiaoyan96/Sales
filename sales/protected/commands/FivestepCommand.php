6c0c53afc481766afdd6a69678d994218c3631ee
anxiaxia <1286571499@qq.com>
2019-03-26 16:28:05 +0800

    离职人员删除五部曲

---
+++ sales/protected/commands/FivestepCommand.php
@@ -0,0 +1,19 @@
+<?php
+class FivestepCommand extends CConsoleCommand
+{
+    public function run()
+    {
+        $suffix = Yii::app()->params['envSuffix'];
+        $sql="select a.id ,a.filename from sales$suffix.sal_fivestep a 
+	          left outer join security$suffix.sec_user b on a.username=b.username
+	          where b.status='I'";
+        $arr=Yii::app()->db->createCommand($sql)->queryRow();
+        foreach ($arr as $a){
+            unlink($a['filename']);
+            $sql1="delete from sales$suffix.sal_fivestep where id = '".$a['id']."'";
+            $command=Yii::app()->db->createCommand($sql1)->execute();
+            $sql2="delete from sales$suffix.sal_fivestep_info where five_id = '".$a['id']."'";
+            $commands=Yii::app()->db->createCommand($sql2)->execute();
+        }
+    }
+}
