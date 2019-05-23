5b3ca7a6f23b917509de80819d44d94bc4093ea9
anxiaxia <1286571499@qq.com>
2019-05-23 11:59:35 +0800

    排行页面

--- sales/protected/config/dashboard.php
+++ sales/protected/config/dashboard.php
@@ -2,7 +2,7 @@
 
 return (array(
 	array('view'=>'notification', 'row'=>0, 'width'=>'col-md-6', 'access'=>'CN04'),
-	array('view'=>'sample', 'row'=>0, 'width'=>'col-md-6', 'access'=>'CN06'),
+	array('view'=>'salelist', 'row'=>0, 'width'=>'col-md-6', 'access'=>'CN06'),
 ));
 
 ?>
