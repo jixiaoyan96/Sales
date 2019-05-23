64e2a82c4437450d47f857f58d918ec261358198
anxiaxia <1286571499@qq.com>
2019-05-23 11:29:15 +0800

    销售排行

---
+++ sales/protected/views/dashboard/salelist.php
@@ -0,0 +1,55 @@
+<?php
+    $suffix = Yii::app()->params['envSuffix'];
+      //城市
+    $sql = "select code,name from security$suffix.sec_city ";
+    $rows = Yii::app()->db->createCommand($sql)->queryAll();
+    $arr=array();
+    foreach ($rows as $a){
+         //人数
+        $sql1="select distinct  username FROM sal_visit  WHERE city='".$a['code']."'";
+        $people = Yii::app()->db->createCommand($sql1)->queryAll();
+        $peoples=count($people);
+        //单数
+        $sql2="select id from sal_visit where city='".$a['code']."' and  visit_obj like '%10%'";
+        $sum = Yii::app()->db->createCommand($sql2)->queryAll();
+        $sums=count($sum);
+        //分数
+        $sale=$sums/($peoples==0?1:$peoples);
+        $sale=round($sale,2)*100;
+        $sale=$sale."%";
+        $model['city']=$a['name'];
+        $model['renjun']=$sale;
+        $arr[]=$model;
+    }
+$last_names = array_column($arr,'renjun');
+array_multisort($last_names,SORT_DESC,$arr);
+
+
+?>
+<div class="box">
+
+    <div  >
+        <table border="1" style="width: 49%;text-align: center;">
+            <tr><td>排名</td><td>城市</td><td>人均签单</td>
+            <?php for($i=0;$i<count($arr);$i++) {?>
+                <tr> <?php if($i<(count($arr)/2)){?>  <td><?php echo $i+1;?></td><td><?php echo $arr[$i]['city'];?></td><td><?php echo $arr[$i]['renjun'];}?></td>
+
+               </tr>
+            <?php }?>
+        </table>
+        <table  border="1" style="width: 49%;float: right;margin-top:-335px ;text-align: center;" >
+            <tr><td>排名</td><td>城市</td><td>人均签单</td>
+                <?php for($i=0;$i<count($arr);$i++) {?>
+            <tr>
+                <?php if($i>(count($arr)/2-1)){?>  <td><?php echo $i+1;?></td><td><?php echo $arr[$i]['city'];?></td><td><?php echo $arr[$i]['renjun'];}?></td>
+            </tr>
+            <?php }?>
+        </table>
+    </div>
+    <div>
+
+    </div>
+    <!-- /.box-body -->
+</div>
+<!-- /.box -->
+</div>
