56aaea26a651ee77a9e0cb58c2a5a9c2d6152825
LBS IT <percylee@lbsgroup.com.hk>
2019-05-30 12:57:38 +0800

    Percy -解決 销售个人签单总金额排行榜凍結主頁問題

--- sales/protected/controllers/DashboardController.php
+++ sales/protected/controllers/DashboardController.php
@@ -24,7 +24,7 @@ class DashboardController extends Controller
 	{
 		return array(
 			array('allow',  // allow all users to perform 'index' and 'view' actions
-				'actions'=>array('notify'),
+				'actions'=>array('notify','salepeople'),
 				'users'=>array('@'),
 			),
 			array('deny',  // deny all users
@@ -41,6 +41,56 @@ class DashboardController extends Controller
 		}
 		echo json_encode($rtn);
 	}
+	
+public function actionSalepeople() {
+    $suffix = Yii::app()->params['envSuffix'];
+    $models=array();
+    $sql="select distinct  a.username ,c.employee_name ,b.name  FROM sal_visit a 
+          inner join hr$suffix.hr_binding c on a.username = c.user_id 
+          inner join security$suffix.sec_city b on a.city = b.code 
+          ";
+    //人名
+    $people = Yii::app()->db->createCommand($sql)->queryAll();
+
+    foreach ($people as $a){
+        $sql1="select id from sal_visit where username='".$a['username']."' and  visit_obj like '%10%'";
+        $id = Yii::app()->db->createCommand($sql1)->queryAll();
+        //区域
+        $sql="select name from security$suffix.sec_city where code=(select region from security$suffix.sec_city where name='".$a['name']."')";
+        $rows = Yii::app()->db->createCommand($sql)->queryAll();
+        //单数
+        //$sums=count($id);
+        $money=0;
+        $moneys=0;
+        foreach ($id as $b){
+            $sql2="select field_id, field_value from sal_visit_info where field_id in ('svc_A7','svc_B6','svc_C7','svc_D6','svc_E7','svc_F4','svc_G3') and visit_id = '".$b['id']."'";
+            $array = Yii::app()->db->createCommand($sql2)->queryAll();
+            $summoney = 0;
+            foreach($array as $item){
+                $summoney += $item['field_value'];
+            }
+            //总金额
+            $money+=$summoney;
+        }
+        $moneys+=$money;
+        if(!empty($rows)){
+            $model['quyu']=$rows[0]['name'];
+        }else{
+            $model['quyu']='空';
+        }
+        $model['name']=$a['employee_name'];
+        $model['user']=$a['username'];
+        $model['city']=$a['name'];;
+        $model['money']=$moneys;
+        $models[]=$model;
+        $last_names = array_column($models,'money');
+        array_multisort($last_names,SORT_DESC,$models);
+        $models = array_slice($models, 0, 20);
+	}
+	
+	echo json_encode($models);
+}
+
 }
 
 ?>
--- sales/protected/views/dashboard/salepeople.php
+++ sales/protected/views/dashboard/salepeople.php
@@ -1,55 +1,3 @@
-<?php
-    $suffix = Yii::app()->params['envSuffix'];
-    $models=array();
-    $sql="select distinct  a.username ,c.employee_name ,b.name  FROM sal_visit a 
-          inner join hr$suffix.hr_binding c on a.username = c.user_id 
-          inner join security$suffix.sec_city b on a.city = b.code 
-          ";
-    //人名
-    $people = Yii::app()->db->createCommand($sql)->queryAll();
-
-    foreach ($people as $a){
-        $sql1="select id from sal_visit where username='".$a['username']."' and  visit_obj like '%10%'";
-        $id = Yii::app()->db->createCommand($sql1)->queryAll();
-        //区域
-        $sql="select name from security$suffix.sec_city where code=(select region from security$suffix.sec_city where name='".$a['name']."')";
-        $rows = Yii::app()->db->createCommand($sql)->queryAll();
-        //单数
-        //$sums=count($id);
-        $money=0;
-        $moneys=0;
-        foreach ($id as $b){
-            $sql2="select field_id, field_value from sal_visit_info where field_id in ('svc_A7','svc_B6','svc_C7','svc_D6','svc_E7','svc_F4','svc_G3') and visit_id = '".$b['id']."'";
-            $array = Yii::app()->db->createCommand($sql2)->queryAll();
-            $summoney = 0;
-            foreach($array as $item){
-                $summoney += $item['field_value'];
-            }
-            //总金额
-            $money+=$summoney;
-        }
-        $moneys+=$money;
-        if(!empty($rows)){
-            $model['quyu']=$rows[0]['name'];
-        }else{
-            $model['quyu']='空';
-        }
-        $model['name']=$a['employee_name'];
-        $model['user']=$a['username'];
-        $model['city']=$a['name'];;
-        $model['money']=$moneys;
-        $models[]=$model;
-        $last_names = array_column($models,'money');
-        array_multisort($last_names,SORT_DESC,$models);
-        $models = array_slice($models, 0, 20);
-    }
-
-//print_r('<pre/>');
-//print_r($models);
-
-?>
-
-
 <div class="box box-primary" >
     <div class="box-header with-border">
         <h3 class="box-title">销售个人签单总金额排行榜</h3>
@@ -62,15 +10,10 @@
     <!-- /.box-header -->
 
     <div class="box-body">
-        <div id='notify' class="direct-chat-messages" style="height: 250px;">
-            <table class="table table-bordered small">
-                <tr><td><b>排名</b></td><td><b>城市</b></td><td><b>区域</b></td><td><b>姓名</b></td><td><b>签单总金额</b></td>
-                    <?php for($i=0;$i<count($models);$i++) {?>
-                <tr>
-                   <td style="color:<?php if($i==0){echo '#FF0000';}if($i==1){echo '#871F78';}if($i==2){echo '#0000FF';}?>"  ><?php echo $i+1;?></td><td style="color:<?php if($i==0){echo '#FF0000';}if($i==1){echo '#871F78';}if($i==2){echo '#0000FF';}?>"><?php echo $models[$i]['city'];?></td><td style="color:<?php if($i==0){echo '#FF0000';}if($i==1){echo '#871F78';}if($i==2){echo '#0000FF';}?>"><?php echo $models[$i]['quyu'];?></td><td style="color:<?php if($i==0){echo '#FF0000';}if($i==1){echo '#871F78';}if($i==2){echo '#0000FF';}?>"><?php echo $models[$i]['name'];?></td><td  style="color:<?php if($i==0){echo '#FF0000';}if($i==1){echo '#871F78';}if($i==2){echo '#0000FF';}?>"><?php echo $models[$i]['money'];?></td>
-                </tr>
-            <?php }?>
-            </table>
+        <div id='salepeople' class="direct-chat-messages" style="height: 250px;">
+            <div class="overlay">
+              <i class="fa fa-refresh fa-spin"></i>
+            </div>
         </div>
     </div>
     <!-- /.box-body -->
@@ -82,3 +25,41 @@
 </div>
 <!-- /.box -->
 
+<?php
+$link = Yii::app()->createAbsoluteUrl("dashboard/salepeople");
+$js = <<<EOF
+	$.ajax({
+		type: 'GET',
+		url: '$link',
+		success: function(data) {
+			if (data !== undefined && data.length != 0) {
+				var line = '<table class="table table-bordered small">';
+                line += '<tr><td><b>排名</b></td><td><b>城市</b></td><td><b>区域</b></td><td><b>姓名</b></td><td><b>签单总金额</b></td></tr>';
+				
+				for (var i=0; i < data.length; i++) {
+					line += '<tr>';
+					style = '';
+					switch(i) {
+						case 0: style = 'style="color:#FF0000"'; break;
+						case 1: style = 'style="color:#871F78"'; break;
+						case 2: style = 'style="color:#0000FF"'; break;
+					}
+					rank = i+1;
+					line += '<td '+style+'>'+rank+'</td><td '+style+'>'+data[i].city+'</td><td '+style+'>'+data[i].quyu+'</td><td '+style+'>'+data[i].name+'</td><td '+style+'>'+data[i].money+'</td>';
+					line += '</tr>';
+				}	
+				
+				line += '</table>';
+				$('#salepeople').html(line);
+			}
+		},
+		error: function(xhr, status, error) { // if error occured
+			var err = eval("(" + xhr.responseText + ")");
+			console.log(err.Message);
+		},
+		dataType:'json'
+	});
+EOF;
+Yii::app()->clientScript->registerScript('salepeopleDisplay',$js,CClientScript::POS_READY);
+
+?>
