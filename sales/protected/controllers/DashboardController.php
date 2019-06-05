8a25175b80c8bea6ccebbb71a6fa2592b712c523
anxiaxia <1286571499@qq.com>
2019-06-05 10:00:26 +0800

    签单人数计算公式

--- sales/protected/controllers/DashboardController.php
+++ sales/protected/controllers/DashboardController.php
@@ -172,7 +172,7 @@ foreach ($models as $key=>$item) {
                 $sql = "select a.city, a.username, sum(convert(b.field_value, decimal(12,2))) as money
 				from sal_visit a force index (idx_visit_02), sal_visit_info b
 				where a.id=b.visit_id and b.field_id in ('svc_A7','svc_B6','svc_C7','svc_D6','svc_E7','svc_F4','svc_G3') 
-				and a.visit_dt >= '$time' and  a.visit_obj like '%10%' and a.city='$code'
+				and a.visit_dt >= '$time'  and a.city='$code'
 				group by a.city, a.username 
 			";
                 $records = Yii::app()->db->createCommand($sql)->queryAll();
