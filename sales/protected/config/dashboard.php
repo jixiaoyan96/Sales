4ea3ce3f52e6a5a69997b8e5a144a9dcf64bdb43
LBS IT <percylee@lbsgroup.com.hk>
2019-06-20 11:52:56 +0800

    Percy - 修改

--- sales/protected/config/dashboard.php
+++ sales/protected/config/dashboard.php
@@ -2,7 +2,7 @@
 
 return (array(
 	array('view'=>'notification', 'row'=>0, 'width'=>'col-md-6', 'access'=>'CN04'),
-    array('view'=>'salepeople', 'row'=>0, 'width'=>'col-md-6', 'access'=>'CN07'),
+    array('view'=>'salepeople', 'row'=>0, 'width'=>'col-md-6', 'access'=>''),
 	array('view'=>'salelist', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
     array('view'=>'salelists', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
 
