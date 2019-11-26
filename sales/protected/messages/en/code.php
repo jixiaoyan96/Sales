cf2ae257c6390b5ec0ddcfe34e70e074a8140f65
LBS IT <percylee@lbsgroup.com.hk>
2019-11-26 18:48:04 +0800

    英文顯示

---
+++ sales/protected/messages/en/code.php
@@ -0,0 +1,36 @@
+<?php
+return array(
+//	'Report Category'=>'报表类别',
+	
+//	'Description'=>'名称',
+//	'Code'=>'编号',
+//	'Name'=>'名称',
+//	'Type'=>'类别',
+//    'Year'=>'年',
+//    'Month'=>'月',
+    'Sum'=>'Mth. Target (No.)',		//'本月业绩目标单数',
+    'Sums'=>'Mth. Target ($)',		//'本月业绩目标金额',
+    'Spanning'=>'Cross-Region % (Active)',	//'跨区提成比例',
+    'Otherspanning'=>'Cross-Region % (Passive)',	//'被跨区提成比例',
+	
+//	'Visit Type'=>'拜访类型',
+//	'Visit Type List'=>'拜访类型列表',
+//	'Visit Type Form'=>'拜访类型表单',
+
+//	'Visit Objective'=>'拜访目的',
+//	'Visit Objective List'=>'拜访目的列表',
+//	'Visit Objective Form'=>'拜访目的表单',
+
+//	'Customer Type'=>'客户类别',
+//	'Customer Type List'=>'客户类别列表',
+//	'Customer Type Form'=>'客户类别表单',
+
+//	'Customer District'=>'客户区域',
+//	'Customer District List'=>'客户区域列表',
+//	'Customer District Form'=>'客户区域表单',
+
+    'Performance'=>'Cross-Region Target',	//'跨区业绩目标',
+    'Performance List'=>'Cross-Region Target List',		//'跨区业绩目标列表',
+    'Performance Form'=>'Cross-Region Target Form',		//'跨区业绩目标表单',
+);
+?>
---
+++ sales/protected/messages/en/customer.php
@@ -0,0 +1,45 @@
+<?php
+return array(
+//	'Customer'=>'客户',
+//	'Customer Form'=>'客户表单',
+//	'Customer List'=>'客户列表',
+
+	'AI'=>'IA Client',	//'IA 客户',
+	'BI'=>'IB Client',	//'IB 客户',
+	'BAF'=>'BAF Client',	//'BAF 客户',
+	'CI'=>'Puriscent Client',	//'飘盈香客户',
+	'INV'=>'INV Client',	//'INV 客户',
+//	'Restaurant'=>'餐饮',
+//	'Non-restaurant'=>'非餐饮',
+
+//	'Code'=>'客戶编号',
+//	'Type'=>'类别',
+//	'Name'=>'名称',
+//	'Full Name'=>'全称',
+//	'Contact Name'=>'客户联系',
+//	'Contact Phone'=>'客户电话',
+//	'Nature'=>'性质',
+//	'Service'=>'服务',
+//	'Frequency'=>'服务次数',
+//	'Frequency'=>'服务次数',
+//	'Monthly Amt'=>'月金額',
+//	'Sign Date'=>'签约日期',
+//	'Contract Period'=>'合同年限',
+//	'Status' => '状态',
+//	'Address'=>'客户地址',
+	
+//	'Customer Code'=>'客户编号',
+//	'Customer Name'=>'客户名字',
+//    'Customer Type'=>'客户类别',
+//	'Date'=>'日期',
+//	'Type'=>'类型',
+//	'Product'=>'服务',
+//	'First Date'=>'首次日期',
+//	'Amount'=>'金额',
+	
+//	'Active'=>'生效中',
+//	'Terminated'=>'停止服务',
+//	'Unknown'=>'不明',
+//	'All'=>'全部',
+);
+?>
