c1d6c20ce82dc0ad9f790114cf4b16f2634538df
anxiaxia <1286571499@qq.com>
2020-03-04 10:15:42 +0800

    五部曲邮件日期判断修改

--- sales/protected/commands/TimerCommand.php
+++ sales/protected/commands/TimerCommand.php
@@ -10,7 +10,7 @@ class TimerCommand extends CConsoleCommand {
         $records = Yii::app()->db->createCommand($sql)->queryAll();
         if (count($records) > 0) {
             foreach ($records as $k=>$record) {
-                $record['start_time'] = date_format(date_create($record['start_time']), "Y/m/d");
+                $record['entry_time'] = date_format(date_create($record['entry_time']), "Y/m/d");
                 $sql1="select a.*, b.name as city_name, f.name as staff_name, f.code as staff_code, 
 				d.field_value as mgr_score, e.field_value as dir_score, g.field_value as sup_score
 				from sales$suffix.sal_fivestep a 
@@ -22,7 +22,7 @@ class TimerCommand extends CConsoleCommand {
 				left outer join sales$suffix.sal_fivestep_info g on a.id=g.five_id and g.field_id='sup_score'
 where f.name= '".$record['name']."'";
                 $arr = Yii::app()->db->createCommand($sql1)->queryAll();
-                if($record['start_time'] == "$firstDay"){
+                if($record['entry_time'] == "$firstDay"){
                     $sql = "select approver_type,username from account$suffix.acc_approver where city='".$record['city']."' and approver_type='regionMgr'";
                     $rows = Yii::app()->db->createCommand($sql)->queryAll();
                     $zjl = $rows[0]['username'];
@@ -33,7 +33,7 @@ where f.name= '".$record['name']."'";
                     $to_addr = "[\"" .$rs[0]['email']."\"]";
                     $subject = "五部曲提醒-" . $record['name'];
                     $description = "五部曲提醒-" . $record['name'];
-                    $message = "姓名：" . $record['name'] . ",入职日期为：" . $record['start_time'] . ",已经到上传五部曲的时间了";
+                    $message = "姓名：" . $record['name'] . ",入职日期为：" . $record['entry_time'] . ",已经到上传五部曲的时间了";
                     $lcu = "admin";
                     $aaa = Yii::app()->db->createCommand()->insert("swoper$suffix.swo_email_queue", array(
                         'request_dt' => date('Y-m-d H:i:s'),
@@ -46,7 +46,7 @@ where f.name= '".$record['name']."'";
                         'lcu' => $lcu,
                         'lcd' => date('Y-m-d H:i:s'),
                     ));
-                }elseif (empty($arr)&&$record['start_time'] == "$secondDay"){
+                }elseif (empty($arr)&&$record['entry_time'] == "$secondDay"){
                     $sql = "select approver_type,username from account$suffix.acc_approver where city='".$record['city']."' and approver_type='regionMgr'";
                     $rows = Yii::app()->db->createCommand($sql)->queryAll();
                     $zjl = $rows[0]['username'];
@@ -57,7 +57,7 @@ where f.name= '".$record['name']."'";
                     $to_addr = "[\"" .$rs[0]['email']."\"]";
                     $subject = "五部曲提醒-" . $record['name'];
                     $description = "五部曲提醒-" . $record['name'];
-                    $message = "姓名：" . $record['name'] . ",入职日期为：" . $record['start_time'] . ",賬戶五部曲還是空白请提醒上传";
+                    $message = "姓名：" . $record['name'] . ",入职日期为：" . $record['entry_time'] . ",賬戶五部曲還是空白请提醒上传";
                     $lcu = "admin";
                     $aaa = Yii::app()->db->createCommand()->insert("swoper$suffix.swo_email_queue", array(
                         'request_dt' => date('Y-m-d H:i:s'),
