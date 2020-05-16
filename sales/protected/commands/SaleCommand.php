e8d484f961b9087ff742947794acb0953c1b31b5
anxiaxia <1286571499@qq.com>
2020-05-16 08:59:30 +0800

    离职销售人员数据转移列表
    拜访记录员工需要显示离职同事姓名

--- sales/protected/commands/SaleCommand.php
+++ sales/protected/commands/SaleCommand.php
@@ -4,9 +4,9 @@ class SaleCommand extends CConsoleCommand
     public function run()
     {
         $suffix = Yii::app()->params['envSuffix'];
-        $sql="UPDATE sales$suffix.sal_visit a ,security$suffix.sec_city b ,security$suffix.sec_user c 
-              set a.username=b.incharge , a.shift='Y'          
-              WHERE a.city=b.code AND a.username=c.username AND a.city=c.city AND c.status='I'";
+        $sql="UPDATE sales$suffix.sal_visit a ,security$suffix.sec_user c 
+              set a.shift='Y'          
+              WHERE  a.username=c.username AND a.city=c.city AND c.status='I'";
         $command=Yii::app()->db->createCommand($sql)->execute();
     }
 }
