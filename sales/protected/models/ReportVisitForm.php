d3894cfc02d457f9a1954c14d066b42093904738
anxiaxia <1286571499@qq.com>
2021-01-18 09:31:39 +0800

    成交率报表查询

--- sales/protected/models/ReportVisitForm.php
+++ sales/protected/models/ReportVisitForm.php
@@ -2066,11 +2066,11 @@ class ReportVisitForm extends CReportForm
 
     public function TurnoverDate($man,$a,$start_dt,$end_dt){
         $att=array();
-        $sql1="select *  from sal_visit  where username='$man' and service_type like '%$a%'  and visit_dt >= '$start_dt'and visit_dt <= '$end_dt' group by cust_name";
+        $sql1="select cust_name from sal_visit  where username='$man' and service_type like '%$a%'  and visit_dt >= '$start_dt'and visit_dt <= '$end_dt' group by cust_name";
         $ai = Yii::app()->db->createCommand($sql1)->queryAll();
         if(empty($ai)){$ai=0;}
         $ai=count($ai);
-        $sql2="select *  from sal_visit  where username='$man' and service_type like '%$a%' and visit_obj like '%10%'  and visit_dt >= '$start_dt'and visit_dt <= '$end_dt' group by cust_name";
+        $sql2="select cust_name  from sal_visit  where username='$man' and service_type like '%$a%' and visit_obj like '%10%'  and visit_dt >= '$start_dt'and visit_dt <= '$end_dt' group by cust_name";
         $b = Yii::app()->db->createCommand($sql2)->queryAll();
         if(empty($b)){$b=0;}
         $b=count($b);
