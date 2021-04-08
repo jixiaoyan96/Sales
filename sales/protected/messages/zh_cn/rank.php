488fa394a220a2e5a78350e1486fbc813c9596ec
anxiaxia <1286571499@qq.com>
2021-04-08 16:15:36 +0800

    说明

--- sales/protected/messages/zh_cn/rank.php
+++ sales/protected/messages/zh_cn/rank.php
@@ -1,9 +1,9 @@
 <?php
 return array(
-	'Season'=>'查看賽季数据',
-	'Year'=>'查看年度数据',
-    'Month'=>'查看月度数据',
-    'All'=>'查看总数据',
+	'Season'=>'查看某一赛季的总分',
+	'Year'=>'查看某一年度的总分',
+    'Month'=>'查看某一月份的总分',
+    'All'=>'查看从段威开始首月至当月月份的总分',
     'Sales rank total score list'=>'销售段位总分榜',
     'RankScore Form'=>'销售段位总分榜表单',
 );
