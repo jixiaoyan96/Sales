5724aa665440420429add967d87129fdda323560
anxiaxia <1286571499@qq.com>
2021-04-09 09:49:42 +0800

    文字

--- sales/protected/messages/zh_cn/rank.php
+++ sales/protected/messages/zh_cn/rank.php
@@ -3,7 +3,7 @@ return array(
 	'Season'=>'查看某一赛季的总分',
 	'Year'=>'查看某一年度的总分',
     'Month'=>'查看某一月份的总分',
-    'All'=>'查看从段威开始首月至当月月份的总分',
+    'All'=>'查看从段位开始首月至当月月份的总分',
     'Sales rank total score list'=>'销售段位总分榜',
     'RankScore Form'=>'销售段位总分榜表单',
 );
