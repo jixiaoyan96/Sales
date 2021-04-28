ff4084dedcae9b98b3f5d18c6aed7b9e434ccf8e
anxiaxia <1286571499@qq.com>
2021-04-28 15:48:55 +0800

    测试

--- sales/protected/config/dashboard.php
+++ sales/protected/config/dashboard.php
@@ -5,7 +5,7 @@ return (array(
     array('view'=>'salepeople', 'row'=>0, 'width'=>'col-md-6', 'access'=>''),
 	array('view'=>'salelist', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
     array('view'=>'salelists', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
-     array('view'=>'ranklist', 'row'=>1, 'width'=>'col-md-4', 'access'=>'CN06'),
+     array('view'=>'ranklist', 'row'=>1, 'width'=>'col-md-4', 'access'=>'HD01'),
 ));
 
 ?>
